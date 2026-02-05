32173e1c235ad07ff1a6f88883f862ae87a24b2a
nativenewengland <maxeconte@gmail.com>
2026-02-05 14:51:41 -0800

    Add main menu scene using main_menu assets

---
+++ scripts/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/character_creator.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	push_warning("Options menu is not implemented yet.")
+
+
+func _on_return_button_pressed() -> void:
+	get_tree().quit()
