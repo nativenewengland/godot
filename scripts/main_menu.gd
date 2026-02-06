c32ad8a03a99829f85d6b5005384736e5a4206a2
nativenewengland <maxeconte@gmail.com>
2026-02-06 00:00:20 -0800

    Route start button to character creator

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world_generation_display.tscn")
+	get_tree().change_scene_to_file("res://scenes/character_creator.tscn")
 
 
 func _on_options_button_pressed() -> void:
