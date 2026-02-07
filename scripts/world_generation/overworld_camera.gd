5978a7101eb4d0d4ee524ca1dab3e4c75a74fdac
nativenewengland <maxeconte@gmail.com>
2026-02-06 22:06:02 -0800

    Adjust overworld camera pan and zoom

--- scripts/world_generation/overworld_camera.gd
+++ scripts/world_generation/overworld_camera.gd
@@ -5,14 +5,46 @@ extends Camera2D
 @export var max_zoom: float = 4.0
 @export var move_speed: float = 600.0
 
+const PAN_THRESHOLD: float = 3.0
+
+var _is_panning := false
+var _pan_pointer_index := -1
+var _pan_start_screen := Vector2.ZERO
+var _pan_start_position := Vector2.ZERO
+var _pan_exceeded_threshold := false
+
 func _unhandled_input(event: InputEvent) -> void:
 	var mouse_event := event as InputEventMouseButton
-	if mouse_event == null or not mouse_event.pressed:
+	if mouse_event != null:
+		if mouse_event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			adjust_zoom(zoom_step)
+			return
+		if mouse_event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			adjust_zoom(-zoom_step)
+			return
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT:
+			if mouse_event.pressed:
+				_start_pan(mouse_event.position, -1)
+			else:
+				_end_pan()
+			return
+
+	var touch_event := event as InputEventScreenTouch
+	if touch_event != null:
+		if touch_event.pressed:
+			_start_pan(touch_event.position, touch_event.index)
+		else:
+			_end_pan()
 		return
-	if mouse_event.button_index == MOUSE_BUTTON_WHEEL_UP:
-		adjust_zoom(zoom_step)
-	elif mouse_event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-		adjust_zoom(-zoom_step)
+
+	var drag_event := event as InputEventScreenDrag
+	if drag_event != null:
+		_update_pan(drag_event.position, drag_event.index)
+		return
+
+	var motion_event := event as InputEventMouseMotion
+	if motion_event != null:
+		_update_pan(motion_event.position, -1)
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector2.ZERO
@@ -36,3 +68,28 @@ func adjust_zoom(delta: float) -> void:
 	zoom = Vector2(next_zoom, next_zoom)
 	var mouse_world_after := get_global_mouse_position()
 	global_position += mouse_world_before - mouse_world_after
+
+func _start_pan(screen_position: Vector2, pointer_index: int) -> void:
+	_is_panning = true
+	_pan_pointer_index = pointer_index
+	_pan_start_screen = screen_position
+	_pan_start_position = global_position
+	_pan_exceeded_threshold = false
+
+func _update_pan(screen_position: Vector2, pointer_index: int) -> void:
+	if not _is_panning or _pan_pointer_index != pointer_index:
+		return
+	var delta := screen_position - _pan_start_screen
+	if not _pan_exceeded_threshold and delta.length() > PAN_THRESHOLD:
+		_pan_exceeded_threshold = true
+		get_viewport().set_input_as_handled()
+	if not _pan_exceeded_threshold:
+		return
+	var world_delta := delta / zoom
+	global_position = _pan_start_position - world_delta
+
+func _end_pan() -> void:
+	if not _is_panning:
+		return
+	_is_panning = false
+	_pan_pointer_index = -1
