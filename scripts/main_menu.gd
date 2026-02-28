b81178a1b91de3f5e906316471ab5f2eb3d461b9
nativenewengland <maxeconte@gmail.com>
2026-02-27 20:17:02 -0800

    Add main menu button to open Shattered UI scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,5 +8,9 @@ func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 
+func _on_shattered_pixel_dungeon_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/shattered_pixel_dungeon_windows.tscn")
+
+
 func _on_return_button_pressed() -> void:
 	get_tree().quit()
