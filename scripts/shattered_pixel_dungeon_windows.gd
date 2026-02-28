8c472a02a3ed02ea92ebe9ea3466b516e365299f
nativenewengland <maxeconte@gmail.com>
2026-02-27 20:28:49 -0800

    Rebuild Shattered UI scene using extracted SPD interface assets

--- scripts/shattered_pixel_dungeon_windows.gd
+++ scripts/shattered_pixel_dungeon_windows.gd
@@ -1,93 +1,69 @@
 extends Control
 
-@onready var message_log: RichTextLabel = $MainMargin/MainLayout/RightColumn/ActionWindow/ActionContent/ActionVBox/MessageLog
-@onready var journal_tabs: TabContainer = $MainMargin/MainLayout/LeftColumn/JournalWindow/JournalContent/JournalTabs
-@onready var hero_window: PanelContainer = $MainMargin/MainLayout/LeftColumn/HeroWindow
-@onready var journal_window: PanelContainer = $MainMargin/MainLayout/LeftColumn/JournalWindow
-@onready var inventory_window: PanelContainer = $MainMargin/MainLayout/RightColumn/InventoryWindow
-@onready var action_window: PanelContainer = $MainMargin/MainLayout/RightColumn/ActionWindow
+@onready var message_log: RichTextLabel = $MessageWindow/MessageMargin/MessageLog
 
 func _ready() -> void:
-	_apply_pixel_window_theme()
-	_set_tab_titles()
+	_apply_chrome_theme()
 
 
-func _set_tab_titles() -> void:
-	journal_tabs.set_tab_title(0, "Quests")
-	journal_tabs.set_tab_title(1, "Lore")
-
-
-func _apply_pixel_window_theme() -> void:
-	var panel_style := StyleBoxFlat.new()
-	panel_style.bg_color = Color("1a1f2f")
-	panel_style.border_color = Color("6f87a5")
-	panel_style.set_border_width_all(3)
-
-	for panel in [hero_window, journal_window, inventory_window, action_window]:
-		panel.add_theme_stylebox_override("panel", panel_style)
-
+func _apply_chrome_theme() -> void:
 	var button_normal := StyleBoxFlat.new()
-	button_normal.bg_color = Color("273247")
-	button_normal.border_color = Color("88a4c9")
+	button_normal.bg_color = Color("1d2433")
+	button_normal.border_color = Color("7d8ea9")
 	button_normal.set_border_width_all(2)
 
 	var button_pressed := StyleBoxFlat.new()
-	button_pressed.bg_color = Color("3a4a67")
-	button_pressed.border_color = Color("b0caf0")
+	button_pressed.bg_color = Color("2b364a")
+	button_pressed.border_color = Color("a7bedf")
 	button_pressed.set_border_width_all(2)
 
 	var button_hover := button_normal.duplicate()
-	button_hover.bg_color = Color("32415d")
+	button_hover.bg_color = Color("263046")
+
+	for node: Node in get_tree().get_nodes_in_group("spd_button"):
+		if node is Button:
+			_style_button(node as Button, button_normal, button_pressed, button_hover)
 
 	for button: Button in [
-		$MainMargin/MainLayout/RightColumn/InventoryWindow/InventoryContent/InventoryVBox/InventoryGrid/Slot1,
-		$MainMargin/MainLayout/RightColumn/InventoryWindow/InventoryContent/InventoryVBox/InventoryGrid/Slot2,
-		$MainMargin/MainLayout/RightColumn/InventoryWindow/InventoryContent/InventoryVBox/InventoryGrid/Slot3,
-		$MainMargin/MainLayout/RightColumn/InventoryWindow/InventoryContent/InventoryVBox/InventoryGrid/Slot4,
-		$MainMargin/MainLayout/RightColumn/InventoryWindow/InventoryContent/InventoryVBox/InventoryGrid/Slot5,
-		$MainMargin/MainLayout/RightColumn/InventoryWindow/InventoryContent/InventoryVBox/InventoryGrid/Slot6,
-		$MainMargin/MainLayout/RightColumn/ActionWindow/ActionContent/ActionVBox/ActionButtons/SearchButton,
-		$MainMargin/MainLayout/RightColumn/ActionWindow/ActionContent/ActionVBox/ActionButtons/WaitButton,
-		$MainMargin/MainLayout/RightColumn/ActionWindow/ActionContent/ActionVBox/ActionButtons/ZapButton,
+		$Toolbar/ActionButtons/SearchButton,
+		$Toolbar/ActionButtons/WaitButton,
+		$Toolbar/ActionButtons/ZapButton,
+		$InventoryWindow/InventoryMargin/InventoryGrid/Item1,
+		$InventoryWindow/InventoryMargin/InventoryGrid/Item2,
+		$InventoryWindow/InventoryMargin/InventoryGrid/Item3,
+		$InventoryWindow/InventoryMargin/InventoryGrid/Item4,
+		$InventoryWindow/InventoryMargin/InventoryGrid/Item5,
+		$InventoryWindow/InventoryMargin/InventoryGrid/Item6,
+		$InventoryWindow/InventoryMargin/InventoryGrid/Item7,
+		$InventoryWindow/InventoryMargin/InventoryGrid/Item8,
 	]:
-		button.add_theme_stylebox_override("normal", button_normal)
-		button.add_theme_stylebox_override("pressed", button_pressed)
-		button.add_theme_stylebox_override("hover", button_hover)
-		button.add_theme_color_override("font_color", Color("e9f2ff"))
+		_style_button(button, button_normal, button_pressed, button_hover)
 
-	for label: Label in [
-		$MainMargin/MainLayout/LeftColumn/HeroWindow/HeroContent/HeroVBox/HeroTitle,
-		$MainMargin/MainLayout/LeftColumn/HeroWindow/HeroContent/HeroVBox/HeroStats,
-		$MainMargin/MainLayout/RightColumn/InventoryWindow/InventoryContent/InventoryVBox/InventoryTitle,
-		$MainMargin/MainLayout/RightColumn/ActionWindow/ActionContent/ActionVBox/ActionTitle,
+	for label: RichTextLabel in [
+		$HeroWindow/HeroMargin/HeroStats,
+		$JournalWindow/JournalMargin/JournalText,
+		message_log,
 	]:
-		label.add_theme_color_override("font_color", Color("f6f8ff"))
+		label.add_theme_color_override("default_color", Color("d5e1f2"))
 
-	$MainMargin/MainLayout/LeftColumn/JournalWindow/JournalContent/JournalTabs/QuestTab/QuestBody.add_theme_color_override("default_color", Color("d2dcf0"))
-	$MainMargin/MainLayout/LeftColumn/JournalWindow/JournalContent/JournalTabs/LoreTab/LoreBody.add_theme_color_override("default_color", Color("d2dcf0"))
-	message_log.add_theme_color_override("default_color", Color("cfe0ff"))
 
-	var health_fill := StyleBoxFlat.new()
-	health_fill.bg_color = Color("cf5353")
-	$MainMargin/MainLayout/LeftColumn/HeroWindow/HeroContent/HeroVBox/HealthBar.add_theme_stylebox_override("fill", health_fill)
-	$MainMargin/MainLayout/LeftColumn/HeroWindow/HeroContent/HeroVBox/HealthBar.add_theme_stylebox_override("background", button_normal)
-
-	var shield_fill := StyleBoxFlat.new()
-	shield_fill.bg_color = Color("4a87cf")
-	$MainMargin/MainLayout/LeftColumn/HeroWindow/HeroContent/HeroVBox/ShieldBar.add_theme_stylebox_override("fill", shield_fill)
-	$MainMargin/MainLayout/LeftColumn/HeroWindow/HeroContent/HeroVBox/ShieldBar.add_theme_stylebox_override("background", button_normal)
+func _style_button(button: Button, normal: StyleBoxFlat, pressed: StyleBoxFlat, hover: StyleBoxFlat) -> void:
+	button.add_theme_stylebox_override("normal", normal)
+	button.add_theme_stylebox_override("pressed", pressed)
+	button.add_theme_stylebox_override("hover", hover)
+	button.add_theme_color_override("font_color", Color("ecf3ff"))
 
 
 func _on_search_button_pressed() -> void:
-	_append_log("[color=#ffe49a]You search the surrounding area for hidden doors.[/color]")
+	_append_log("[color=#f5e4a6]You search for hidden doors.[/color]")
 
 
 func _on_wait_button_pressed() -> void:
-	_append_log("[color=#b3d6ff]You wait and listen... distant chains rattle in the dark.[/color]")
+	_append_log("[color=#b9d7ff]You wait a turn and listen carefully.[/color]")
 
 
 func _on_zap_button_pressed() -> void:
-	_append_log("[color=#cdb7ff]You channel your wand: a violet spark cracks across the room.[/color]")
+	_append_log("[color=#d2b8ff]A wand spark flashes across the hall.[/color]")
 
 
 func _append_log(entry: String) -> void:
