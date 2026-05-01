1d72d8b7772146385f92531474d10e3b30337546
nativenewengland <maxeconte@gmail.com>
2026-05-01 17:38:17 -0400

    Fix dwarf hold tooltip/click tile coordinate conversion

--- scripts/world_generation/dwarf_hold_generation.gd
+++ scripts/world_generation/dwarf_hold_generation.gd
@@ -1875,7 +1875,8 @@ func _ensure_chest_inventory(cell: Vector2i) -> void:
 	DwarfHoldChestService.ensure_chest_inventory(_chest_inventories, cell, _rng, CHEST_LOOT_TABLE)
 
 func _cell_from_mouse_position(mouse_position: Vector2) -> Vector2i:
-	var local_position := city_layer.to_local(mouse_position)
+	var mouse_global := city_panel.to_global(mouse_position)
+	var local_position := city_layer.to_local(mouse_global)
 	return city_layer.local_to_map(local_position)
 
 func _is_chest_cell(cell: Vector2i) -> bool:
